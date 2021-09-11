95f6b1ae8ed7f485a9e8c00bb749f1316dc923c5
Vinay G B <vinay@synctactic.ai>
2021-09-12 00:44:17 +0530

    Da man, da myth , da legendary setuptools to pack this thing

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as rh:
+    requirements = rh.read().splitlines()
+
+
+setuptools.setup(
+    name="jat-queue",
+    version="0.0.1",
+    author="Vinay G B",
+    author_email="vinaygb665@gmail.com",
+    description="Just another task queue",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/VinayGb665/jat-queue",
+    project_urls={
+        "Bug Tracker": "https://github.com/VinayGb665/jat-queue/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    entry_points ={
+        'console_scripts': [
+            'jatq = executors.main:package_cli'
+        ]
+    },
+    install_requires=requirements,
+    package_dir={"": "./"},
+    packages=setuptools.find_packages(where="./"),
+    python_requires=">=3.9",
+)
